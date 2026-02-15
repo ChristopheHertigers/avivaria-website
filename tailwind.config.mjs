dec3d3f862c28a83495519b8fb08e197e299dcce
Christophe Hertigers <christophe.hertigers@pom.be>
2026-02-15 14:10:34 +0100

    savepoint before restarting

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: ['Roboto', 'sans-serif'],
+            custom: ['Roboto', 'sans-serif']
+        },
+    },
+    plugins: [],
+}
